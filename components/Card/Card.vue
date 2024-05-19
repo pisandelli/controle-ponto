4ce1c2bf1efd203b48a9c8c875509d46cca39020
Pedro Pisandelli <pisandelli@gmail.com>
2024-05-19 12:17:30 -0300

    add simple card

---
+++ components/Card/Card.vue
@@ -0,0 +1,18 @@
+<script lang='ts' setup>
+/**
+* A simple card component.
+* @name 'Simple Card'
+* @version 1.0.0
+*/
+</script>
+
+<template lang="pug">
+BoxL.card
+  slot
+</template>
+
+<style lang="stylus" scoped>
+.card
+  border: 1px solid var(--card_border-color, #ccc )
+  border-radius: var(--border-radius, 0.5rem)
+</style>
