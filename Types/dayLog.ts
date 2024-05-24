a6db3d96713a5589f55a644b6a58a9443a03dda2
Pedro Pisandelli <pisandelli@gmail.com>
2024-05-24 11:33:12 -0300

    feat(pinia): Add first structure for Store
    Closes #14

--- Types/dayLog.ts
+++ Types/dayLog.ts
@@ -1,13 +1,15 @@
 export type DayLog = {
-    key: string
-    entrada: string
-    pausaInicio: string
-    pausaFim: string
-    saida: string
+    [key: string]: number | boolean | {} | null
+    entrada: number | null
+    pausaInicio: number | null
+    pausaFim: number | null
+    saida: number | null
+    soma: number | null
     obs: {
-      entrada: string
-      pausaInicio: string
-      pausaFim: string
-      saida: string
+      [key: string]: string | null
+      entrada: string | null
+      pausaInicio: string | null
+      pausaFim: string | null
+      saida: string | null
     }
 }
